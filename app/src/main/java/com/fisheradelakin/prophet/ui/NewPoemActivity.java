e1ef80419258773bfe4731ea9ffaad817ddf0797
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-02-20 12:57:05 -0600

    nothing works

--- app/src/main/java/com/fisheradelakin/prophet/ui/NewPoemActivity.java
+++ app/src/main/java/com/fisheradelakin/prophet/ui/NewPoemActivity.java
@@ -1,9 +1,16 @@
 package com.fisheradelakin.prophet.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,10 +18,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.fisheradelakin.prophet.R;
 import com.fisheradelakin.prophet.model.Poem;
+import com.fisheradelakin.prophet.util.ErrorDialog;
+import com.fisheradelakin.prophet.util.StorageConfirmationDialog;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -28,11 +41,14 @@ import io.realm.RealmResults;
 public class NewPoemActivity extends AppCompatActivity {
 
     private static final String TAG = "NPA";
+    private static final int WRITE_STORAGE_PERMISSION = 1;
+    public static final String FRAGMENT_DIALOG = "dialog";
     private Realm mRealm;
 
     @Bind(R.id.title_et) EditText mTitleET;
     @Bind(R.id.poem_et) EditText mPoemET;
     @Bind(R.id.author_et) EditText mAuthorET;
+    @Bind(R.id.poem_view) RelativeLayout mPoemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,9 +101,56 @@ public class NewPoemActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             savePoem();
             return true;
+        } else if (id == R.id.action_share) {
+            sharePoem();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static Bitmap loadBitmapFromView(View v, int width, int height) {
+        Bitmap b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(0, 0, v.getLayoutParams().width, v.getLayoutParams().height);
+        v.draw(c);
+        return b;
+    }
+
+    private void sharePoem() {
+
+        Bitmap bitmap = loadBitmapFromView(mPoemView, mPoemView.getWidth(), mPoemView.getHeight());
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestWriteStoragePermission();
+        } else {
+            File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Prophet");
+
+            if (!folder.exists()) {
+                folder.mkdir();
+                Log.i(TAG, "Folder doesn't exist but it does now.");
+            } else {
+                Log.i(TAG, "Folder exists");
+            }
+
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+            File file = new File(folder, "prophet" + timeStamp + ".png");
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+                fileOutputStream.flush();
+                fileOutputStream.close();
+                file.setReadable(true, false);
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+                intent.setType("image/png");
+                startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void savePoem() {
@@ -118,6 +181,27 @@ public class NewPoemActivity extends AppCompatActivity {
                 super.onError(e);
             }
         });
+    }
+
+    private void requestWriteStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new StorageConfirmationDialog().show(getFragmentManager(), FRAGMENT_DIALOG);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_STORAGE_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == WRITE_STORAGE_PERMISSION) {
+            if (grantResults.length != 1 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                ErrorDialog.newInstance(getString(R.string.write_permission))
+                        .show(getFragmentManager(), FRAGMENT_DIALOG);
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
 }
